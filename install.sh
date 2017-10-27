15569a108ff173c117491a645ebbffd9494172bc
Iheanyi Ekechukwu <iheanyi@digitalocean.com>
2017-10-27 16:05:46 -0400

    Nicer refactoring of the script.

--- install.sh
+++ install.sh
@@ -1,3 +1,26 @@
+#!/bin/sh
+# Software for provisioning up a Macbook Pro and all that good stuff.
+brew_install() {
+    if test ! $(brew list | grep $package); then
+      brew install "$@"
+    else
+        echo '$package already installed, gonna skip that.'
+    fi
+}
+
+cask_install() {
+    if test ! $(brew cask list | grep $application); then
+      brew install "$@"
+    else
+        echo '$application already installed, gonna skip that.'
+    fi
+}
+
+copy_configs() {
+    cp .nvimrc ~
+    cp .tmux.conf ~
+}
+
 # Install Homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
@@ -5,19 +28,75 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 
-# Install various libraries
-brew install wget
-brew install curl
-brew install git
-brew install jq
-brew install autojump
+packages=(
+    wget
+    curl
+    git
+    jq
+    autojump
+    python3
+    the_silver_searcher
+    fzf
+    tmate
+    highlight
+    hugo
+    vim
+    emacs-plus
+    tmux
+    neovim
+)
 
-# Install Support for Various Languages
-brew install elixir
-brew install go
+for package in "$packages[@]"
+  do brew_install $package
+done
 
-# Install other tooling
-brew install docker
+copy_configs()
+
+languages=(
+    elixir
+    go
+    ruby
+)
+
+for package in "$languages[@]"
+  do brew_install $package
+done
+
+# Install brew caskroom
+brew tap caskroom/cask
+brew tap caskroom/fonts
+
+# Install applications
+applications=(
+    1password
+    alfred
+    dash
+    caffeine
+    google-chrome
+    firefox
+    fantastical
+    spectacle
+    skype
+    iterm2
+    figma
+    cyberduck
+    insomnia
+    flux
+    timing
+    licecap
+    atom
+    the-unarchiver
+    slack
+    sketch
+    docker
+    spotify
+    sequel-pro
+    font-source-code-pro
+)
+
+for application in "$applications[@]"
+  do cask_install $application
+done
 
 # Install Node Version Manager (NVM)
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash
@@ -27,7 +106,6 @@ nvm install stable
 nvm alias default stable
 
 # Install Neovim
-brew install neovim
 
 # Install and Move Vim Colors Stuff
 mkdir ~/.vim/colors
@@ -36,9 +114,6 @@ mv Tomorrow-Night-Eighties.vim ~/.vim/colors
 
 # Install Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-cp .nvimrc ~
-cp .tmux.conf ~
 
 # Install Pure
 npm install --global pure-prompt
