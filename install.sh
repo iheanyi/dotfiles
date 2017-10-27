5a995b8ec15cac1ffd8958a70322d2ebcba5c849
Iheanyi Ekechukwu <iheanyi@digitalocean.com>
2017-10-27 15:06:06 -0400

    Update scripts and tmux configuration.

--- install.sh
+++ install.sh
@@ -8,14 +8,20 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 # Install various libraries
 brew install wget
 brew install curl
-brew install git # version control software
+brew install git
+brew install jq
+brew install autojump
 
 # Install Support for Various Languages
 brew install elixir
+brew install go
 
+# Install other tooling
+brew install docker
 
 # Install Node Version Manager (NVM)
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash
+
 # Install latest
 nvm install stable
 nvm alias default stable
@@ -28,7 +34,6 @@ mkdir ~/.vim/colors
 curl https://raw.githubusercontent.com/chriskempson/tomorrow-theme/master/vim/colors/Tomorrow-Night-Eighties.vim
 mv Tomorrow-Night-Eighties.vim ~/.vim/colors 
 
-
 # Install Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
@@ -37,3 +42,6 @@ cp .tmux.conf ~
 
 # Install Pure
 npm install --global pure-prompt
+
+# Install Spacemacs
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
