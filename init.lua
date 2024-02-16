b7d710686e44e2f1998d8a3711c479023ad5e06b
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2024-02-15 23:02:02 -0500

    Surround settings

--- init.lua
+++ init.lua
@@ -63,7 +63,21 @@ require("lazy").setup({
   },
 
 
-  { 'echasnovski/mini.surround', version = false },
+  {
+    'echasnovski/mini.surround',
+    version = false,
+    opts = {
+      mappings = {
+        add = "gsa", -- Add surrounding in Normal and Visual modes
+        delete = "gsd", -- Delete surrounding
+        find = "gsf", -- Find surrounding (to the right)
+        find_left = "gsF", -- Find surrounding (to the left)
+        highlight = "gsh", -- Highlight surrounding
+        replace = "gsr", -- Replace surrounding
+        update_n_lines = "gsn", -- Update `n_lines`
+      },
+    }
+  },
 
   -- commenting out lines
   {
