4e943a4c7a6d66540cf81c1c9cb2673ac59f182f
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2024-02-17 14:09:30 -0500

    Lower timeout_ms

--- init.lua
+++ init.lua
@@ -124,7 +124,7 @@ require("lazy").setup({
     opts = {
       format_on_save = {
         -- These options will be passed to conform.format()
-        timeout_ms = 500,
+        timeout_ms = 200,
         lsp_fallback = true,
       },
       formatters_by_ft = {
