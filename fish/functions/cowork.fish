b5d7d63a643b507dba4834f539a953a4c702e563
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2024-02-27 13:55:40 -0500

    Add fish functions.

---
+++ fish/functions/cowork.fish
@@ -0,0 +1,4 @@
+function cowork --wraps='npx @koddsson/coworking-with' --description 'alias cowork=npx @koddsson/coworking-with'
+  npx @koddsson/coworking-with $argv
+        
+end
---
+++ fish/functions/ss.fish
@@ -0,0 +1,4 @@
+function ss --wraps='bundle exec spring stop' --description 'alias ss=bundle exec spring stop'
+  bundle exec spring stop $argv
+        
+end
