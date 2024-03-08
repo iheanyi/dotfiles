d5a498d3e2264230acbdde7eb55a5de681eed508
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2024-03-08 17:21:20 -0500

    Editor is neovim

--- config.fish
+++ config.fish
@@ -8,7 +8,7 @@ end
 set -gxp PATH $HOME/development/go/bin /opt/homebrew/bin /opt/homebrew/opt/openjdk/bin
 set -gx GOBIN $HOME/development/go/bin
 
-set -gx EDITOR vim
+set -gx EDITOR nvim
 set -gx TERM tmux-256color
 set -gx COLORTERM truecolor
 set -gx FZF_DEFAULT_OPTS '--color=bg+:#3B4252,bg:#2E3440,spinner:#81A1C1,hl:#616E88,fg:#D8DEE9,header:#616E88,info:#81A1C1,pointer:#81A1C1,marker:#81A1C1,fg+:#D8DEE9,prompt:#81A1C1,hl+:#81A1C1'
